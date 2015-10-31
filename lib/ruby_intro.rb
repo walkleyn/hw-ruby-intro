6aa504a31ce79a67e7af02c6fc75c9fe95298b23
Walter Kleynscheldt <walkleyn@gmail.com>
2015-10-31 15:15:04 +0000

    Parts1 and 2 pass

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,30 +2,57 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  if arr.empty?
+    0
+  else 
+    arr.inject { |sum,x| sum + x}
+  end
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  if arr.empty?
+    0
+  elsif arr.size == 1
+    arr[0]
+  else
+    sorted = arr.sort.reverse
+    sorted[0] + sorted[1]
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  arr.each_index do |index|
+    arr.each do |element|
+      if element + arr[index] == n
+        return true unless element == arr[index]
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  consonants = ['A','E','I','O','U']
+  if s.length == 0 or s =~ /\W+/
+    return false
+  else
+    return true unless consonants.include?(s[0].upcase)
+  end
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  if s =~ /\D+/ or s.length == 0
+    false
+  else
+    return true if s.to_i % 4 == 0
+  end
 end
 
 # Part 3
